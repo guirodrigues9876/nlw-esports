b0dd6a8783cd469886b6b59ea001db5bedc0b84f
Guilherme Rodrigues <guilherme121225@gmail.com>
2022-09-15 17:20:56 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from 'react-native';
+import { useFonts} from 'expo-google-fonts/inter';
+
+
+import { Background } from './src/components/Background' ;
+
+export default function App() {
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
+  );
+}
+
